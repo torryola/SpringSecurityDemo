214b6710e6dcc2a0846828aeaf0a94228933ac73
torryola <54688905+torryola@users.noreply.github.com>
2022-12-25 18:04:12 +0000

    Database-Auth-Implementation

---
+++ stock-management-system/src/main/java/net/torrydev/stockmanagementsystem/authentication/AppUserDetailsDaoImp.java
@@ -0,0 +1,34 @@
+package net.torrydev.stockmanagementsystem.authentication;
+
+import net.torrydev.stockmanagementsystem.model.AppUserDetails;
+import net.torrydev.stockmanagementsystem.model.AppUserRole;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository("DemoDb") // This is a Qualifier for this repo impl - Makes it easier to switch impl of the interface
+public class AppUserDetailsDaoImp implements AppUserDetailsDao{
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public AppUserDetailsDaoImp(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public Optional<AppUserDetails> findAppUserByUserName(String userName) {
+        return getUsers().stream().filter(appUserDetails -> userName.equals(appUserDetails.getUsername())).findFirst();
+    }
+
+    // Hard-Coded User details for brevity. This can be replaced with actual JPA repository method or
+    // any other Db implementation
+    private List<AppUserDetails> getUsers(){
+        return List.of(new AppUserDetails(AppUserRole.ADMIN.getSimpleGrantedAuthorities(), passwordEncoder.encode("Password"), "admin",Boolean.TRUE, Boolean.TRUE, Boolean.TRUE, Boolean.TRUE),
+                new AppUserDetails(AppUserRole.DEVELOPER.getSimpleGrantedAuthorities(), passwordEncoder.encode("Password"), "dev",Boolean.TRUE, Boolean.TRUE, Boolean.TRUE, Boolean.TRUE),
+                new AppUserDetails(AppUserRole.USER.getSimpleGrantedAuthorities(), passwordEncoder.encode("Password"), "user",Boolean.TRUE, Boolean.TRUE, Boolean.TRUE, Boolean.TRUE));
+    }
+}
--- stock-management-system/src/main/java/net/torrydev/stockmanagementsystem/security/AppSecurityConfig.java
+++ stock-management-system/src/main/java/net/torrydev/stockmanagementsystem/security/AppSecurityConfig.java
@@ -1,22 +1,18 @@
 package net.torrydev.stockmanagementsystem.security;
 
+import net.torrydev.stockmanagementsystem.authentication.AppUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-
-import static net.torrydev.stockmanagementsystem.model.AppUserRole.*;
 
 @Configuration
 @EnableWebSecurity
@@ -26,6 +22,14 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
     private static final String[] SECURE_URL_PATTERN = {"/api/v1/secured/**"};
     private static final String[] OPEN_URL_PATTERN = {"/api/v1/open/**"};
     public static final String SECURE_DEV_URL_PATTERN = "/api/v1/secure/**";
+
+    private final AppUserDetailsService appUserDetailsService;
+    private final PasswordEncoder passwordEncoder;
+
+    public AppSecurityConfig(AppUserDetailsService appUserDetailsService, PasswordEncoder passwordEncoder) {
+        this.appUserDetailsService = appUserDetailsService;
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -53,36 +57,16 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
 //                 .rememberMe(); // Enable Remember Option in the login
     }
 
-
-    // For Custom userName and Password - Using InMemoryUserDetails
-
-    @Override @Bean
-    public UserDetailsService userDetailsServiceBean() throws UsernameNotFoundException {
-        // Build Custom Userdetails
-        UserDetails adminUser = User.builder()
-                .username("admin")
-                .password(passwordEncoder().encode("Password")) // You must encode the password
-                //.roles(ADMIN.name())
-                .authorities(ADMIN.getSimpleGrantedAuthorities())
-                .build();
-        UserDetails nonAdminUser = User.builder()
-                .username("User")
-                .password(passwordEncoder().encode("Password")) // You must encode the password
-                //.roles(USER.name())
-                .authorities(USER.getSimpleGrantedAuthorities())
-                .build();
-        UserDetails dev = User.builder()
-                .username("Dev")
-                .password(passwordEncoder().encode("Password")) // You must encode the password
-                //.roles(DEVELOPER.name())
-                .authorities(DEVELOPER.getSimpleGrantedAuthorities())
-                .build();
-        return new InMemoryUserDetailsManager(adminUser, nonAdminUser, dev);
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(getDbAuthenticationProvider());
     }
 
-    // Password Encryption is expected by default in Spring Security - Not creating will result in something like "There is no PasswordEncoder mapped for "
     @Bean
-    public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder(10);
+    public AuthenticationProvider getDbAuthenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder);
+        provider.setUserDetailsService(appUserDetailsService);
+        return provider;
     }
 }
