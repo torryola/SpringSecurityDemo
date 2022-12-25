e2f2173de4307a3e1c9c710284d705c93d28644a
torryola <54688905+torryola@users.noreply.github.com>
2022-12-25 18:41:42 +0000

    Migrate AppSecurityConfig to New Component-Based Security Config without WebSecurityConfigurerAdapter

--- stock-management-system/src/main/java/net/torrydev/stockmanagementsystem/security/AppSecurityConfigWithoutWebSecurityConfigAdapter.java
+++ stock-management-system/src/main/java/net/torrydev/stockmanagementsystem/security/AppSecurityConfigWithoutWebSecurityConfigAdapter.java
@@ -4,6 +4,7 @@ import net.torrydev.stockmanagementsystem.authentication.AppUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -12,14 +13,13 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-//@Configuration
-//@EnableWebSecurity
-//@EnableGlobalMethodSecurity(prePostEnabled = true)
-@Deprecated(since = "WebSecurityConfigurerAdapter is deprecated " +
-        "New Component-Based Security Config is the new approach")
-public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class AppSecurityConfigWithoutWebSecurityConfigAdapter {
     private static final String[] PUBLIC_ALLOW_URL_PATTERN = {"/", "index","/login", "/css/*", "/js/*"};
     private static final String[] SECURE_URL_PATTERN = {"/api/v1/secured/**"};
     private static final String[] OPEN_URL_PATTERN = {"/api/v1/open/**"};
@@ -28,44 +28,34 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
     private final AppUserDetailsService appUserDetailsService;
     private final PasswordEncoder passwordEncoder;
 
-    public AppSecurityConfig(AppUserDetailsService appUserDetailsService, PasswordEncoder passwordEncoder) {
+    public AppSecurityConfigWithoutWebSecurityConfigAdapter(AppUserDetailsService appUserDetailsService, PasswordEncoder passwordEncoder) {
         this.appUserDetailsService = appUserDetailsService;
         this.passwordEncoder = passwordEncoder;
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                //.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()) // So Spring doesn't check Request Header for csrf_token
                 .csrf().disable()
-                .authorizeRequests()
-                .antMatchers(PUBLIC_ALLOW_URL_PATTERN).permitAll() // Whitelist any url with open and Home i.e. / or index.html
+                .authorizeHttpRequests(authReq -> authReq.antMatchers(PUBLIC_ALLOW_URL_PATTERN).permitAll() // Whitelist any url with open and Home i.e. / or index.html
                 .anyRequest()
-                .authenticated()
-                .and()
-                //.httpBasic(); // Enabling Basic Authentication - Doesn't have logout
+                .authenticated())
                 .formLogin() // Enabling Form-Based Authentication - Has logout
-                    .loginPage("/login").permitAll() // Custom Login UI
-                    .defaultSuccessUrl("/products/catalog", true) // On Successful login, user will be redirected to that page instead of default "/" or index
-                    .and()
-                    .logout().logoutUrl("/logout")
+                .loginPage("/login").permitAll() // Custom Login UI
+                .defaultSuccessUrl("/products/catalog", true) // On Successful login, user will be redirected to that page instead of default "/" or index
+                .and()
+                .logout().logoutUrl("/logout")
                 .logoutRequestMatcher(new AntPathRequestMatcher("/logout", HttpMethod.GET.name())) // POST req method should be set, If CSRF is enabled, otherwise GET.
                 // We use Get because csrf is disabled for this security config
-                        .clearAuthentication(true)
-                        .deleteCookies("JSESSIONID","remember-me")
-                        .invalidateHttpSession(true)
-                        .logoutSuccessUrl("/login");
-//                .and()
-//                 .rememberMe(); // Enable Remember Option in the login
-    }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(getDbAuthenticationProvider());
+                .clearAuthentication(true)
+                .deleteCookies("JSESSIONID","remember-me")
+                .invalidateHttpSession(true)
+                .logoutSuccessUrl("/login");
+        return http.build();
     }
 
     @Bean
-    public AuthenticationProvider getDbAuthenticationProvider() {
+    public AuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
         provider.setPasswordEncoder(passwordEncoder);
         provider.setUserDetailsService(appUserDetailsService);
